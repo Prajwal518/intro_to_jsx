96ea3fa5e3af0d06f3a9bba02957ff1431164934
Prajwal Gowda S <65961288+Prajwal518@users.noreply.github.com>
2021-02-12 10:42:12 +0530

    you can add two elements (h1/p) in a single render by using a div around them

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 var React = require("react");
 var ReactDom = require("react-dom");
 
-ReactDom.render(<h1>hello there</h1>, document.getElementById("root"));
+ReactDom.render(
+<div>
+  <h1>hello there</h1>
+  <p>its zenith here</p>
+</div>, 
+document.getElementById("root"));
